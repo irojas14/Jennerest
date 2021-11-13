3d271752fd75fb6335b205ad8c57b9a76ad97e4d
jiolivar <javier.olivares.13@sansano.usm.cl>
2021-11-13 12:48:07 -0300

    2 power ups mas

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
-    
+    // Powerup_1 = saltar mas alto
+    // Powerup_2 = buffear el disparo
+    // Powerup_3 = Moverse mas rapido
+        
     private void OnTriggerEnter2D(Collider2D collision) 
     {
         
         if (collision.tag=="Player")
         {
-            // Acceder al script del player,  aunque no me funca ayuda jesu
+            // Acceder al script del player,  aunque no me funca ayuda jesus
             /*
             GameObject player = collision.gameObject;
             Player playerScript = player.GetComponent<PlayerController>();
@@ -23,7 +26,7 @@ public class PowerUp : MonoBehaviour
     }
 
     void Pickup()
-    {
+    {   
         Destroy(this.gameObject);
     }
 
