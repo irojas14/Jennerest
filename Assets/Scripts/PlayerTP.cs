6ca9fe05f03fcc654e778214330634ad0b133f36
jiolivar <javier.olivares.13@sansano.usm.cl>
2021-11-21 21:18:15 -0300

    teleporter implementado

---
+++ Assets/Scripts/PlayerTP.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTP : MonoBehaviour
+{
+    private GameObject currentTeleporter;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (currentTeleporter != null)
+            {
+                transform.position  = currentTeleporter.GetComponent<Teleporter>().GetDestination().position;
+            }
+        }
+    }
+
+    // Importante que las puertas tengan el tag "Teleporter"
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Teleporter"))
+        {
+            currentTeleporter = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) 
+    {
+        if (collision.CompareTag("Teleporter"))
+        {
+            currentTeleporter = null;
+        }
+    }
+}
